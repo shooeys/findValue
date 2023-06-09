91a447b7fc60063c65ab9764bad6a0ece7730a8d
shooeys <133713818+shooeys@users.noreply.github.com>
2023-06-09 12:18:24 -0400

    Update and rename findValue.js to index.html

--- findValue.js
+++
@@ -1,7 +0,0 @@
-function getValueFromSpreadsheet() {
-    var sheet = SpreadsheetApp.openById('1892219054').getSheetByName('Sheet1');
-    var value = sheet.getRange(309, 2).getValue();
-    return value;
-  }
-  
-  console.log(value);
